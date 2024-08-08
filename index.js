09ea3336623c6674a1f946a46f5541478bf8e8f4
Gerard Ingles <ger.ing.mon@gmail.com>
2024-08-08 14:35:50 +0200

    phonebook backend 3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 
 let persons = [
