456abf2ae0cc4704a60b993ce6f0cee7d2154559
Gerard Ingles <ger.ing.mon@gmail.com>
2024-08-12 11:55:23 +0200

    Phonebook backend ex. 3.13 - 3.14

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
@@ -34,24 +36,27 @@ let persons = [
 ]
 
 app.get('/info', (request, response) => {
-    const totalPersons = persons.length
-    const timeRequest = new Date();
+  let totalPersons = 0
+  const timeRequest = new Date()
+
+  Person.find({}).then(persons => {
+    totalPersons = persons.length
     response.send('<p>Phonebook has info for ' + totalPersons + ' people</p><p>' + timeRequest + '</p>')
+  })
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
- 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }    
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  }).catch(error => {
+    response.status(404).end()
+  })
 })
 
 const generateId = () => {
@@ -61,38 +66,34 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
     if(!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'Name or number missing'
-        })
+        return response.status(400).json({ error: 'Name or number missing' })
     }
 
-    const duplicate = persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())
+    /*const duplicate = persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())
 
     if (duplicate) {
       return response.status(400).json({
         error: 'Name must be unique'
       })
-    }
+    }*/
 
-    const newPerson = {
+    const newPerson = new Person({
         name: body.name,
-        number: body.number,
-        id: generateId()
-    }
-  
-    persons = persons.concat(newPerson)
-
-    response.json(newPerson)
+        number: body.number
+    })
+    
+    newPerson.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id).then(person => {
+      response.status(204).end()
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
